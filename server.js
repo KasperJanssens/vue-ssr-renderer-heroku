88d6864b533888781e563e3123b9794356a4d2ce
Kasper <Kasper@krondorsoft.com>
2018-03-11 18:39:22 +0000

    switched off serving of vue

--- server.js
+++ server.js
@@ -20,13 +20,13 @@ const app = express();
 app.use('/dist', express.static('dist'));
 
 // Render all other routes with the bundleRenderer.
-app.get('*', (req, res) => {
-  bundleRenderer
-    // Renders directly to the response stream.
-    // The argument is passed as "context" to main.server.js in the SSR bundle.
-    .renderToStream({url: req.path})
-    .pipe(res);
-});
+// app.get('*', (req, res) => {
+//   bundleRenderer
+//     // Renders directly to the response stream.
+//     // The argument is passed as "context" to main.server.js in the SSR bundle.
+//     .renderToStream({url: req.path})
+//     .pipe(res);
+// });
 
 // Bind the app to this port.
 app.listen(8080);
